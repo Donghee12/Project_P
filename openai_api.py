99d7f4352f0f0b4d5cfa4786598cb7c2d27806ca
김동희 <khho119@naver.com>
2024-11-13 16:45:43 +0900

    api 추가

---
+++ openai_api.py
@@ -0,0 +1,5 @@
+#openai의 api를 불러오는 코드입니다.
+#openai의 api를 사용하기 위해서는 openai의 api key가 필요합니다.
+#openai의 api key는 openai의 홈페이지에서 받을 수 있습니다.
+#openai의 api를 사용하기 위해서는 openai의 python sdk를 설치해야합니다.
+#openai의 python sdk는 pip install openai로 설치할 수 있습니다.
