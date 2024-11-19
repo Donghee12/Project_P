7d13917060b40b6a6457d0a872e5ba0196d0b612
김동희 <khho119@naver.com>
2024-11-19 20:08:08 +0900

    로딩화면 추가, html 디자인 수정

---
+++ static/js/script.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", function() {
+    // 페이지 로드가 끝난 후 2초 동안 로딩 화면 유지
+    setTimeout(function() {
+        const loader = document.getElementById('loader');
+        const content = document.getElementById('content');
+        
+        // 로딩 화면 숨기기
+        loader.style.display = 'none';
+        
+        // 콘텐츠 표시
+        content.style.display = 'block';
+    }, 3000); // 2000ms = 2초
+});
